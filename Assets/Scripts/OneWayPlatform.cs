a458dc080962216eef7297d61e43f41b30bdc136
Adam Waggoner <adam.swaggoner@gmail.com>
2015-10-23 20:34:31 -0600

    yup[

---
+++ Assets/Scripts/OneWayPlatform.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneWayPlatform : MonoBehaviour {
+    public Collider2D ignore;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Physics2D.IgnoreCollision(ignore, other, true);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Physics2D.IgnoreCollision(ignore, other, false);
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileBehavior: MonoBehaviour {
+    public Vector2 InitialVelocity = new Vector2(1,1);
+    public float ExplodeTime = 1f;
+    public GameObject Explosion;
+
+    private float ExplodeTimer_;
+
+    Rigidbody2D body;
+    // Use this for initialization
+	void Start () {
+        body = gameObject.GetComponent<Rigidbody2D>();
+        body.AddForce(InitialVelocity);
+        ExplodeTimer_ = ExplodeTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ExplodeTimer_ -= Time.deltaTime;
+
+        if (ExplodeTimer_ <= 0)
+        {
+            Destroy(this.gameObject);
+            GameObject.Destroy(GameObject.Instantiate(Explosion, transform.position, Quaternion.identity) as GameObject,.5f);
+        }
+	}
+}
